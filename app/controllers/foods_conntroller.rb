940ac48a14f9d0f610d3a5a55431fc42e0f88d44
yamashita-takahiro-44 <shimolen@gmail.com>
2023-07-28 13:26:05 +0900

    mvp

--- app/controllers/foods_conntroller.rb
+++
@@ -1,9 +0,0 @@
-class FoodsController < ApplicationController
-  def search
-    puts params[:query] # この行を追加
-    service = FatSecretApiService.new
-    @foods = service.search_food(params[:query])
-
-    render json: @foods
-  end
-end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,23 @@
 CarrierWave.configure do |config|
-  # if Rails.env.production?
-    config.storage    = :aws
-    config.aws_bucket = ENV.fetch('AWS_S3_BUCKET')
-    config.aws_acl    = 'public-read'
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('AWS_S3_BUCKET')
+  config.aws_acl    = 'public-read'
  
-    # The maximum period for authenticated_urls is only 7 days.
-    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+  # The maximum period for authenticated_urls is only 7 days.
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
  
-    # Set custom options such agit s cache control to leverage browser caching
-    config.aws_attributes = {
-      expires: 1.week.from_now.httpdate,
-      cache_control: 'max-age=604800'
-    }
+  # Set custom options such as cache control to leverage browser caching
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  }
  
-    # aws credential
-    config.aws_credentials = {
-      # 今回はIAM ロールを使用するため記載しない
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region: ENV.fetch('AWS_REGION')
-    }
-  # else
-    # テスト時はローカルにファイルを保存する
-    # config.storage = :file
-  # end
+  # AWS credentials
+  config.aws_credentials = {
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region: ENV.fetch('AWS_REGION')
+  }
 end
  
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
